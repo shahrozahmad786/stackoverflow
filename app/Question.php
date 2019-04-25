4da181ee88962e64fbba9af7017bfda52ce99228
Shahroz <shahrozahmad363@gmail.com>
2019-04-25 17:05:46 +0500

    Generating Model

---
+++ app/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+
+   protected $guarded=[];
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+
+    public function setTitleAttribute($value)
+    {
+    	$this->attributes['title']=$value;
+    	$this->attributes['slug']= str_slug($value);
+
+    }
+}
